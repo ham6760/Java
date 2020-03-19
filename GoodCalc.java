6ce4182d3d63d05e78bd5a97809a02ae486b9e1e
ham6760 <ham6760@naver.com>
2020-03-19 15:57:06 +0900

    java

--- GoodCalc.java
+++ GoodCalc.java
@@ -9,9 +9,10 @@ public class GoodCalc extends Calculator{
 	}
 	public double average(int[] a) {
 		double sum=0;
-		for (int i = 0; i < a.length; i++)
-			sum += a[i];
+		for (int i = 0; i < a.length; i++) 
+			sum += a[i];			
 		return sum/a.length;
+	
 	}
 	public static void main(String[] args) {
 		GoodCalc c=new GoodCalc();
@@ -19,4 +20,5 @@ public class GoodCalc extends Calculator{
 		System.out.println(c.subtract(2, 3));
 		System.out.println(c.average(new int [] {2,3,4}));
 	}
+
 }
