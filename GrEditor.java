3cf2be3017068629ec4acf9bc7fd1945e994288a
ham6760 <ham6760@naver.com>
2020-03-26 16:06:17 +0900

    java5

---
+++ GrEditor.java
@@ -0,0 +1,10 @@
+
+
+public class GrEditor extends Circle{
+	public static void main(String[] args){
+		Shape shape=new Shape();
+		Shape circle=new Circle();
+		circle.draw();
+		shape.draw();
+	}
+}
