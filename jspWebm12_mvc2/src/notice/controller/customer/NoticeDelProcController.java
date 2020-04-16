0e78286e79cdb75d357287c5f252416f77c0388b
ham6760 <ham6760@naver.com>
2020-04-16 15:03:58 +0900

    jsp12

---
+++ jspWebm12_mvc2/src/notice/controller/customer/NoticeDelProcController.java
@@ -0,0 +1,35 @@
+package notice.controller.customer;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import notice.controller.Controller;
+import notice.dao.NoticeDao;
+import notice.vo.Notice;
+
+public class NoticeDelProcController implements Controller{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("NoticeDelProcController 신호");
+		
+		String seq=request.getParameter("c");
+
+		Notice n=new Notice();
+		NoticeDao dao=new NoticeDao();
+
+		int af=dao.delete(seq);
+		
+		PrintWriter out=response.getWriter();
+		//어디로 가라
+		if(af>0){//삭제성공	
+			response.sendRedirect("notice.jsp");
+		}else{//삭제실패	
+			out.write("삭제 오류");
+		}
+		
+	}
+
+}
